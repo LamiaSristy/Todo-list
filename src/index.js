e1f8d113e024f4ee4f17b0bf314d0af1553a7dd6
Muhammad Talha Waqar <talhawaqar007@gmail.com>
2020-12-15 19:06:27 +0500

    add bootstrap and other dependencies belongs to it

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
+import 'bootstrap';
+import './style/main.scss';;
 
-function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+// function component() {
+//   const element = document.createElement('div');
+
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
