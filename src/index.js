fdc8378712d2abaffff03ae8ac2098a4bd24dc3a
Muhammad Talha Waqar <talhawaqar007@gmail.com>
2020-12-17 15:14:57 +0500

    Added functionalities for task creation

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ const { Todo } = require('./todoClass');
 const createProjectBtn = document.getElementById('btnCreateProject');
 const projectsulEl = document.getElementById('projects-ul');
 const todoDisplayEl = document.getElementById('tododisplay');
+const btnAddTask = document.getElementById('btnAddTask');
+const projectIndexTask = document.getElementById('projectIndexTask');
+const btnCreateTask = document.getElementById('btnCreateTask');
 
 let projects = [];
 
@@ -26,46 +29,46 @@ function setDefaults() {
 }
 setDefaults();
 
-const createNewProject = (project) => {
+const createNewProject = (project, index) => {
   const liEl = document.createElement('li');
   const lianchorEl = document.createElement('a');
   lianchorEl.textContent = project.title;
-  lianchorEl.id = lianchorEl.textContent;
+  lianchorEl.id = `index-${index}`;
 
-  lianchorEl.addEventListener('click', () => {
+  lianchorEl.addEventListener('click', (e) => {
     document.querySelector(".active-project").classList.remove('active-project');
-
+    projectIndexTask.value = (e.srcElement.id).split('-')[1];
     lianchorEl.classList.add('active-project');
+    document.getElementById('createTaskDiv').classList.add('d-block');
     // lianchorEl.id = lianchorEl.textContent;
     todoDisplayEl.innerHTML = '';
-    for(let j = 0; j < project.items.length; j += 1) {        
+    for (let j = 0; j < project.items.length; j += 1) {
       const todoLiEl = document.createElement('li');
-      const title = document.createElement('button');    
-      const panel = document.createElement('div');  
+      const title = document.createElement('button');
+      const panel = document.createElement('div');
       const description = document.createElement('p');
-      
-      
+
       title.classList.add('accordion');
       panel.classList.add('panel');
-    
+
       title.textContent = project.items[j].title;
       description.textContent = project.items[j].description;
 
       panel.appendChild(description);
-      
-      todoDisplayEl.appendChild(todoLiEl);      
+
+      todoDisplayEl.appendChild(todoLiEl);
       todoLiEl.appendChild(title);
       todoLiEl.appendChild(panel);
 
       title.addEventListener('click', () => {
         title.classList.toggle("active");
-        
+
         if (panel.style.display === "block") {
           panel.style.display = "none";
         } else {
           panel.style.display = "block";
         }
-      });      
+      });
     }
   });
 
@@ -75,15 +78,15 @@ const createNewProject = (project) => {
 
 const getDataFromLocalStorage = () => {
   projects = JSON.parse(localStorage.getItem('todolist'));
-  projectsulEl.innerHTML='';
+  projectsulEl.innerHTML = '';
   for (let i = 0; i < projects.length; i += 1) {
-    let liEl = createNewProject(projects[i]);
-    if (i==0) liEl.getElementsByTagName('a')[0].classList.add('active-project');
+    let liEl = createNewProject(projects[i], i);
+    if (i == 0) liEl.getElementsByTagName('a')[0].classList.add('active-project');
 
     projectsulEl.appendChild(liEl);
-  } 
+  }
 
-  console.log(projects);
+  //  console.log(projects);
 }
 
 getDataFromLocalStorage();
@@ -96,9 +99,24 @@ const updateLocalStorage = () => {
 createProjectBtn.addEventListener('click', function () {
   const projectName = document.getElementById('projectName').value;
   const newProject = new Project(projectName);
-  projects.push(newProject);
 
+  console.log(projects.length);
+  projects.push(newProject);
   updateLocalStorage();
   $('#projectModal').modal('hide');
-  createNewProject(newProject);
+  createNewProject(newProject, projects.length - 1);
+});
+
+btnCreateTask.addEventListener('click', function () {
+  let taskName = document.getElementById('taskName').value;
+  let description = document.getElementById('description').value;
+  let dueDate = document.getElementById('dueDate').value;
+  let priority = document.getElementById('priority').value;
+
+  let toDo = new Todo(taskName, description, dueDate, priority);
+  let projectIndex = projectIndexTask.value;
+  projects[projectIndex].items.push(toDo);
+  updateLocalStorage();
+  console.log(projects);
+  $('#taskModal').modal('hide');
 });
