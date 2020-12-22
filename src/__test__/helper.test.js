55847dc80548e9aaebeaa32b0e2827d28ef1be0a
Muhammad Talha Waqar <talhawaqar007@gmail.com>
2020-12-22 14:08:01 +0500

    Fix lint errors and improve test

---
+++ src/__test__/helper.test.js
@@ -0,0 +1,21 @@
+const { getDataFromLocalStorage, updateLocalStorage } = require('../helper');
+
+test('Returns projects which is null initially', () => {
+  expect(getDataFromLocalStorage('projects')).toBeNull();
+});
+
+test('Upload Localstorage', () => {
+  const projects = [];
+  updateLocalStorage(projects);
+  expect(updateLocalStorage('projects')).toBeUndefined();
+});
+
+test('Check the Key for local storage', () => {
+  localStorage.setItem.key = 'todolist';
+  expect(localStorage.setItem.key).toEqual('todolist');
+});
+
+test('Upload Localstorage', () => {
+  localStorage.setItem.value = 'project';
+  expect(localStorage.setItem.value).toEqual('project');
+});
